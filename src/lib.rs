362eb637d08a7fcb6a16f5283c11b86e1ea6c597
iurii-kyrylenko <yuriproject@gmail.com>
2020-07-14 14:24:54 +0300

    Implement LinearImageData struct

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,46 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub struct LinearImageData {
+    width: usize,
+    height: usize,
+    rgbas: Vec<u8>,
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Test wasm-canvas!");
+impl LinearImageData {
+    pub fn new(width: usize, height: usize) -> Self {
+        Self {
+            width,
+            height,
+            rgbas: vec![0; 4 * width * height],
+        }
+    }
+
+    pub fn put_pixel(&mut self, x: usize, y: usize, r: u8, g: u8, b: u8, a: u8) {
+        let offset = 4 * (self.width * y + x);
+        self.rgbas[offset + 0] = r;
+        self.rgbas[offset + 1] = g;
+        self.rgbas[offset + 2] = b;
+        self.rgbas[offset + 3] = a;
+    }
+
+    pub fn pixels(&self) -> *const u8 {
+        self.rgbas.as_ptr()
+    }
+
+    pub fn frame_example(&mut self, t: usize) {
+        for i in 0..self.width {
+            for j in 0..self.height {
+                self.put_pixel(
+                    (i + t) % self.width,
+                    j,
+                    (i % 256) as u8,
+                    (j % 256) as u8,
+                    (i * j % 256) as u8,
+                    (i * j % 256) as u8
+                );
+            }
+        }
+    }
 }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,41 @@
-import * as wasm from "wasm-canvas";
+import { memory } from "wasm-canvas/wasm_canvas_bg";
+import { LinearImageData } from "wasm-canvas";
 
-wasm.greet();
+const width = 1240;
+const height = 580;
+
+const canvas = document.getElementById("canvas");
+canvas.width = width;
+canvas.height = height;
+const ctx = canvas.getContext('2d');
+
+const linearImageData = LinearImageData.new(width, height);
+const pixels = linearImageData.pixels();
+
+const arr = new Uint8ClampedArray(memory.buffer, pixels, 4 * width * height);
+const imageData = new ImageData(arr, width);
+
+let t = 0;
+
+const renderLoop = () => {
+  linearImageData.frame_example(t++);
+  // frame_example_2(t++);
+  ctx.putImageData(imageData, 0, 0);
+  requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
+
+function frame_example_2(t) {
+  for (let i = 0; i < width; i++)
+    for (let j = 0; j < height; j++) {
+      linearImageData.put_pixel(
+        (i + t) % width,
+        j,
+        i % 256,
+        j % 256,
+        i * j % 256,
+        i * j % 256
+      );
+    }
+}
